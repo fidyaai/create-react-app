6472978aae36caa1b8c52823b0b6b219132bc3f7
FIDYAAi <fidyaa265@gmail.com>
2022-05-22 15:05:46 +0700

    coba

---
+++ src/Materi/LifecycleComponent/Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import News from './News';
+
+
+export default class News extends React.Component {
+    
+    render () { 
+        console.log('RENDER');
+        return (          
+            <div>
+                <News />
+            </div>
+        )
+    }
+}
